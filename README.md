570b2920821bf78b5751331afff052714ad924e0
hgidha <hgidha@ucsc.edu>
2023-01-09 18:13:54 -0800

    Added comments and Update README

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Calc-GUI
+
 A small project to learn to use the PySimpleGUI library 
+
+# Running Calc
+
+Get the Calc.py file and the lib directory or your own copy of PySimpleGUI and run it
+
+## Files
+
+### calc.py
+
+This file contains the implementaion of the GUI for the calculator using PySimpleGUI
+
+### Lib
+
+This folder contains a copy of the PySimpleGUI library
+
+### PySimpleGUI.py
+
+This file is the PySimpleGUI library in portable form
--- calc.py
+++ calc.py
@@ -1,10 +1,9 @@
-import PySimpleGUI as sg
+import PySimpleGUI as sg #Importing the library 
 
-expr = []
+expr = [] #Creating list to hold the expression 
 
-layout = [[sg.Text(size=(10,1))],
-        [sg.Text(size=(16,1), key='-Output-', font=('Any 18'), text_color='black')],
-        [sg.Text(size=(10,1))],
+#Setting up the layout of the window with the output line as well as the buttons 
+layout = [[sg.Text(size=(16,1), key='-Output-', font=('Any 18'), text_color='black')],
         [sg.ReadFormButton(button_text='C', size=(5,2)), sg.Button(button_text='DEL', size=(5,2))], 
         [sg.Button(button_text=' + ', size=(5,2)), sg.Button(button_text=' - ', size=(5,2)),
         sg.Button(button_text=' * ',  size=(5,2)), sg.Button(button_text=' / ',  size=(5,2))],
@@ -15,26 +14,29 @@ layout = [[sg.Text(size=(10,1))],
         [sg.Button(button_text='0',  size=(5,2)), sg.Button(button_text='1',  size=(5,2)),
         sg.Button(button_text='.',  size=(5,2)), sg.Button(button_text=' = ',  size=(5,2))]]
 
-window = sg.Window("Calculator", layout)
+sg.theme("Dark Amber")
+window = sg.Window("Calculator", layout) #Creating the GUI window 
+
 while True:
-    event, value = window.read()
-    if(event == sg.WIN_CLOSED):
+    event, value = window.read() #Holding the values passed in from reading the window
+    if(event == sg.WIN_CLOSED): #Checks if the user has closed the window 
         break
-    elif(event == 'C'):
-        expr.clear()
+    elif(event == 'C'): #Checks if user has pressed clear 
+        expr.clear() #Empties the list
+        window['-Output-'].update(''.join(expr)) #Clears the output line 
+    elif(event == 'DEL'): #Checks if user pressed delete 
+        if len(expr) > 0: #Checks if list is empty before poping an element
+            expr.pop(len(expr) - 1)
         window['-Output-'].update(''.join(expr))
-    elif(event == 'DEL'):
-        expr.pop(len(expr) - 1)
-        window['-Output-'].update(''.join(expr))
-    elif(event == ' = '):
-        total = eval(''.join(expr))
-        expr.clear()
-        expr.append(str(total))
+    elif(event == ' = '): #Checks if user pressed =
+        total = eval(''.join(expr)) #Evaluates the expression by joining all elements of the list 
+        expr.clear() #Clears the list
+        expr.append(str(total)) #Adds the new value to the list 
         window['-Output-'].update(total)
-    elif(len(expr) == 17):
+    elif(len(expr) == 17): #Checks for overall expr lenght limit
         pass
-    else:
-        expr.append(event)
+    else: #Else a num button or expr button has been pressed
+        expr.append(event) #Add the new expr element to the list
         window['-Output-'].update(''.join(expr))
     
-window.close()
+window.close() #Close the window when finished
